6218289f843186687dd7955c74328bbf53c4b911
jotavecorta <julianvilla.90@gmail.com>
2021-11-09 20:23:45 -0300

    Add graphs.ipynb

---
+++ small_perturbation_method/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+NAME = "spm"
+
+from . import johnson_1999
+from . import spm1
+from . import spm2
+
---
+++ small_slope_aprox/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+NAME = "SSA"
+
+from . import two_rough_layers
