62f787e5fc12e329108e8f152bb360baef2e6ce9
jotavecorta <julianvilla.90@gmail.com>
2022-01-19 20:40:31 -0300

    Add first draft goa.py

---
+++ DL-SM/geometric_optics_approx/goa.py
@@ -0,0 +1,4 @@
+"""Core script for Geometric Optics Approximation module. All the functions were 
+developed based on the equations from RADIO SCIENCE, VOL. 46, RS0E20, 2011"""
+
+import numpy as np
