08524ec7341ba7097fd582225f114bb97b289fd4
jotavecorta <julianvilla.90@gmail.com>
2022-02-03 17:30:40 -0300

    add scattering_amplitudes to goa.py

--- DL-SM/geometric_optics_approx/goa.py
+++ DL-SM/geometric_optics_approx/goa.py
@@ -104,22 +104,81 @@ def global_polarization_vectors(theta_inc, phi_inc, theta, phi):
     incident_pol = {'horizonal': (h_ix, h_iy, h_iz), 'vertical': (v_ix, v_iy, v_iz)}
 
     # Scattered vertical polarization
-    v_sx = -np.cos(theta)*np.cos(phi)
-    v_sy = -np.cos(theta)*np.sin(phi)
-    v_sz = -np.sin(theta)
+    v_x = -np.cos(theta)*np.cos(phi)
+    v_y = -np.cos(theta)*np.sin(phi)
+    v_z = -np.sin(theta)
 
     # Scattered horizantal polarization
-    h_sx = - np.sin(phi)
-    h_sy = np.cos(phi)
-    h_sz = 0
+    h_x = - np.sin(phi)
+    h_y = np.cos(phi)
+    h_z = 0
 
-    scattered_pol = {'horizonal': (h_sx, h_sy, h_sz), 'vertical': (v_sx, v_sy, v_sz)}    
+    scattered_pol = {'horizonal': (h_x, h_y, h_z), 'vertical': (v_x, v_y, v_z)}    
 
     return incident_pol, scattered_pol
              
 
-def kirchhoff_amplitudes(wave_vectors, polarization, fresnel_coeff):
-    pass
+def scattering_amplitudes(wave_vectors, polarization, fresnel_coeff):
+    """N. Pinel, C. Boulier, 'Electromagnetic Wave Scattering from Random 
+    Rough Surfaces: Asymptotic Models', Wiley-ISTE, 2013, page 66, eq. 2.57."""
+    # Unpack vectors
+    k_ix, k_iy, k_iz, k = wave_vectors['incident']
+    k_x, k_y, k_z = wave_vectors['scatter']
+
+    # Surface slopes on MSP
+    gamma_x, gamma_y = slopes(wave_vectors)
+    n_mod = np.sqrt(1 + gamma_x**2 + gamma_y**2)
+
+    # Unpack global polarization
+    incident_pol, scattered_pol = polarization
+
+    h_ix, h_iy, h_iz = incident_pol['horizontal']
+    v_ix, v_iy, v_iz = incident_pol['vertical']    
+
+    h_x, h_y, h_z = scattered_pol['horizontal']
+    v_x, v_y, v_z = scattered_pol['vertical']
+
+    # Local polarization and normal
+    local_pol = local_polarization_vectors(wave_vectors)
+    nx, ny, nz = local_pol['normal']
+    px, py, pz = local_pol['parallel']
+    qx, qy, qz = local_pol['perpendicular']
+
+    # Local Fresnel coefficients
+    Rh = fresnel_coeff['horizontal']
+    Rv = fresnel_coeff['vertical']
+
+    # Horizontal incidence
+    f_hh = n_mod/2 * ( 
+        - (1 - Rh) * (h_ix*qx + h_iy*qy + h_iz*qz) * (nx*k_ix + ny*k_iy + nz*k_iz) * (h_x*qx + h_y*qy + h_z*qz)
+        + (1 + Rv) * (h_ix*px + h_iy*py + h_iz*pz) * (h_x*(ny*qz - nz*qy) + h_y*(nz*qx - nx*qz) + h_z*(nx*qy - ny*qx))
+        + (1 + Rh) * (h_ix*qx + h_iy*qy + h_iz*qz) * ((k_x*qx + k_y*qy + k_z*qz) * (h_x*nx + h_y*ny + h_z*nz) - (k_x*nx + k_y*ny + k_z*nz) * (h_x*qx + h_y*qy + h_z*qz))
+        + (1 - Rv) * (h_ix*px + h_iy*py + h_iz*pz) * (nx*k_ix + ny*k_iy + nz*k_iz) * (h_x*(k_y*qz - k_z*qy) + h_y*(k_z*qx - k_x*qz) + h_z*(k_x*qy - k_y*qx))
+    )
+
+    f_hv = n_mod/2 * ( 
+        - (1 - Rh) * (h_ix*qx + h_iy*qy + h_iz*qz) * (nx*k_ix + ny*k_iy + nz*k_iz) * (v_x*qx + v_y*qy + v_z*qz)
+        + (1 + Rv) * (h_ix*px + h_iy*py + h_iz*pz) * (v_x*(ny*qz - nz*qy) + v_y*(nz*qx - nx*qz) + v_z*(nx*qy - ny*qx))
+        + (1 + Rh) * (h_ix*qx + h_iy*qy + h_iz*qz) * ((k_x*qx + k_y*qy + k_z*qz) * (v_x*nx + v_y*ny + v_z*nz) - (k_x*nx + k_y*ny + k_z*nz) * (v_x*qx + v_y*qy + v_z*qz))
+        + (1 - Rv) * (h_ix*px + h_iy*py + h_iz*pz) * (nx*k_ix + ny*k_iy + nz*k_iz) * (v_x*(k_y*qz - k_z*qy) + v_y*(k_z*qx - k_x*qz) + v_z*(k_x*qy - k_y*qx))
+    )
+
+    # Vertical incidence
+    f_vh = n_mod/2 * ( 
+        - (1 - Rh) * (v_ix*qx + v_iy*qy + v_iz*qz) * (nx*k_ix + ny*k_iy + nz*k_iz) * (h_x*qx + h_y*qy + h_z*qz)
+        + (1 + Rv) * (v_ix*px + v_iy*py + v_iz*pz) * (h_x*(ny*qz - nz*qy) + h_y*(nz*qx - nx*qz) + h_z*(nx*qy - ny*qx))
+        + (1 + Rh) * (v_ix*qx + v_iy*qy + v_iz*qz) * ((k_x*qx + k_y*qy + k_z*qz) * (h_x*nx + h_y*ny + h_z*nz) - (k_x*nx + k_y*ny + k_z*nz) * (h_x*qx + h_y*qy + h_z*qz))
+        + (1 - Rv) * (v_ix*px + v_iy*py + v_iz*pz) * (nx*k_ix + ny*k_iy + nz*k_iz) * (h_x*(k_y*qz - k_z*qy) + h_y*(k_z*qx - k_x*qz) + h_z*(k_x*qy - k_y*qx))
+    )
+
+    f_vv = n_mod/2 * ( 
+        - (1 - Rh) * (v_ix*qx + v_iy*qy + v_iz*qz) * (nx*k_ix + ny*k_iy + nz*k_iz) * (v_x*qx + v_y*qy + v_z*qz)
+        + (1 + Rv) * (v_ix*px + v_iy*py + v_iz*pz) * (v_x*(ny*qz - nz*qy) + v_y*(nz*qx - nx*qz) + v_z*(nx*qy - ny*qx))
+        + (1 + Rh) * (v_ix*qx + v_iy*qy + v_iz*qz) * ((k_x*qx + k_y*qy + k_z*qz) * (v_x*nx + v_y*ny + v_z*nz) - (k_x*nx + k_y*ny + k_z*nz) * (v_x*qx + v_y*qy + v_z*qz))
+        + (1 - Rv) * (v_ix*px + v_iy*py + v_iz*pz) * (nx*k_ix + ny*k_iy + nz*k_iz) * (v_x*(k_y*qz - k_z*qy) + v_y*(k_z*qx - k_x*qz) + v_z*(k_x*qy - k_y*qx))
+    )
+
+    return {'horizontal': (f_hh, f_hv), 'vertical': (f_vv, f_vh)}
 
 def four_fold_integration(theta_i, wave_vectors, slope_pdf, scatter_amplitudes):
     pass
