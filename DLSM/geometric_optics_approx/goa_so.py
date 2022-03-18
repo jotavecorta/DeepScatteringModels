83a6d8c97e161f7d492d494bba4e32d04ca12113
jotavecorta <julianvilla.90@gmail.com>
2022-03-18 17:40:45 -0300

    started goa_2O.sigma_2O

--- DLSM/geometric_optics_approx/goa_so.py
+++ DLSM/geometric_optics_approx/goa_so.py
@@ -4,4 +4,33 @@ from RADIO SCIENCE, VOL. 46, RS0E20, 2011"""
 
 import numpy as np
 
-import goa
+from .goa import *
+
+
+def sigma_2O(wave_vectors, p_slope, amplitudes, shadow):
+    # Create integration variables
+    TH_l, PH_l, TH_m, PH_l = np.meshgrid(
+        np.linspace(1e-5, 90, 30) * np.pi / 180,
+        np.linspace(0, 360, 30) * np.pi / 180,
+        np.linspace(1e-5, 90, 30) * np.pi / 180,
+        np.linspace(0, 360, 30) * np.pi / 180,
+    )
+
+    # Unpack vectors
+    k_ix, k_iy, k_iz, k = wave_vectors["incident"]
+    k_x, k_y, k_z = wave_vectors["reflected"]
+
+    # Unpack Amplitudes
+    f_hh, f_hv = amplitudes["horizontal"]
+    f_vv, f_vh = amplitudes["vertical"]
+
+    # Shadowing function
+    S = 1 if shadow is None else shadow
+
+    # Scattering Cross Section
+    sigma = {
+        f"{pol}": -k * np.pi * abs(f) ** 2 * p_slope * S / (k_z - k_iz) ** 2 / k_iz
+        for pol, f in zip(["hh", "hv", "vv", "vh"], [f_hh, f_hv, f_vv, f_vh])
+    }
+
+    return sigma
