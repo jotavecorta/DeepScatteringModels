9bc6a4491ea7f5677a6e8a7868acb03849563b99
jotavecorta <julianvilla.90@gmail.com>
2022-07-13 20:29:20 -0300

    Add mueller_matrix to t_matrix.py

--- DLSM/small_perturbation_method/t_matrix.py
+++ DLSM/small_perturbation_method/t_matrix.py
@@ -587,3 +587,67 @@ class SpmSurface:
 
         return t_matrix
 
+        def mueller_matrix(self, lambda_, theta_inc, phi_inc, second_order=True, **kwargs):
+            s_matrix_dict = self._spm1_s_matrix(lambda_, theta_inc, phi_inc)
+
+            # Unpack amplitudes
+            s_hh, s_vv, s_hh_vv = s_matrix_dict['co-pol']
+            s_hv, s_hh_hv, s_vv_hv = s_matrix_dict['cross-pol']
+
+            # Second order
+            if second_order:
+                amps_dict = self._spm2_integration(
+                    lambda_, theta_inc, phi_inc, **kwargs)
+
+                # Unpack amplitudes
+                s2_hh, s2_vv, s2_hh_vv = amps_dict['co-pol']
+                s2_hv, s2_hh_hv, s2_vv_hv = amps_dict['cross-pol']
+
+                # Add second order terms
+                s_hh += s2_hh
+                s_vv += s2_vv
+                s_hh_vv += s2_hh_vv
+                s_hv += s2_hv
+                s_hh_hv += s2_hh_hv
+                s_vv_hv += s2_vv_hv
+           
+            # Upper Triangle
+            m_11 = s_hh + s_vv + 2 * s_hv
+
+            m_12 = s_hh - s_vv
+            
+            m_13 = 2 * np.real(s_hh_hv + s_vv_hv)
+
+            m_14 = 2j * np.imag(s_hh_hv - s_vv_hv)
+            
+            m_22 = s_hh + s_vv - 2 * s_hv
+            
+            m_23 = 2 * np.real(s_hh_hv - s_vv_hv)
+
+            m_24 = 2j * np.imag(s_hh_hv + s_vv_hv)
+            
+            m_33 = 2 * (s_hv + np.real(s_hh_vv))
+
+            m_34 = 2j * np.imag(s_hh_vv)
+
+            m_44 = 2 * (s_hv - np.real(s_hh_vv))                
+
+            # Lower Triangle
+            m_21 = m_12
+
+            m_31, m_32 = m_13, m_23
+
+            m_41 = m_14
+
+            m_42, m_43 = m_24, m_34
+
+            # Mueller Matrix
+            m_matrix = np.array([(m_11, m_12, m_13, m_14),
+                          (m_21, m_22, m_23, m_24),
+                          (m_31, m_32, m_33, m_34),
+                          (m_41, m_42, m_43, m_44)])
+
+            return m_matrix
+
+        def polarization_signature(self, lambda_, theta_inc, phi_inc, second_order=True, **kwargs):
+            pass
